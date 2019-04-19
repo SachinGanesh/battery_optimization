bcfaf5b2731f843c70ff9bd4c3cd8cff76b684dc
Sachin Ganesh <sachinganesh@outlook.com>
2019-04-19 14:56:03 +0530

    first commit

---
+++ ios/Classes/BatteryOptimizationPlugin.m
@@ -0,0 +1,20 @@
+#import "BatteryOptimizationPlugin.h"
+
+@implementation BatteryOptimizationPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"battery_optimization"
+            binaryMessenger:[registrar messenger]];
+  BatteryOptimizationPlugin* instance = [[BatteryOptimizationPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
