5d738fc26d3855562d40fdb0c7fc1b3e7e9b5521
ritheshSalyan <rithesh@dreamsoftin.com>
2021-11-23 18:40:52 +0530

    migrated to null safety

--- lib/battery_optimization.dart
+++ lib/battery_optimization.dart
@@ -6,17 +6,17 @@ class BatteryOptimization {
   static const MethodChannel _channel =
       const MethodChannel('battery_optimization');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<bool> isIgnoringBatteryOptimizations() async {
+  static Future<bool?> isIgnoringBatteryOptimizations() async {
     final reading = await _channel.invokeMethod('isIgnoringBatteryOptimizations');
     return reading;
   }
 
-  static Future<String> openBatteryOptimizationSettings() async {
+  static Future<String?> openBatteryOptimizationSettings() async {
     final reading = await _channel.invokeMethod('openBatteryOptimizationSettings');
     return reading;
   }
